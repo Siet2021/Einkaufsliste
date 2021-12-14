2201a9b71b43a1f29a99cc9009085d8d1c4d96c2
Siet2021 <95638024+Siet2021@users.noreply.github.com>
2021-12-14 20:01:01 +0100

    Einkaufsliste

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ let shoppingItems = getShoppingItemsFromLocalStorage();
             document.getElementById('list').innerHTML = '';
 
             for (let index = 0; index < shoppingItems.length; index += 1) {
-                document.getElementById('list').innerHTML += '<li>' + shoppingItems[index] +
-                ' <button onclick="removeItem(' + index + '); updateList();">X</button></li>';
+                document.getElementById('list').innerHTML += '<li onclick="removeItem(' + index + '); updateList();">' + shoppingItems[index] +
+                '</li>';
             }
         }
