6b2b91b77176ed36f0345b2dfd4e8512b478f9c3
Siet2021 <95638024+Siet2021@users.noreply.github.com>
2021-12-14 19:41:11 +0100

    Einkaufsliste

---
+++ script.js
@@ -0,0 +1,40 @@
+let shoppingItems = getShoppingItemsFromLocalStorage();
+
+        updateList();
+
+        function getShoppingItemsFromLocalStorage() {
+            let items = localStorage.getItem('shoppingItems');
+
+            if (items == null || items == '') {
+                items = [];
+            } else {
+                items = items.split(',');
+            }
+
+            return items;
+        }
+
+        function addNewItem() {
+            
+            let item = document.getElementById('inp').value;
+            document.getElementById('inp').innerHTML = '';
+
+            if (item != null) {
+                shoppingItems.push(item);
+                localStorage.setItem('shoppingItems', shoppingItems);
+            }
+        }
+
+        function removeItem(itemIndex) {
+            shoppingItems.splice(itemIndex, 1);
+            localStorage.setItem('shoppingItems', shoppingItems);
+        }
+
+        function updateList() {
+            document.getElementById('list').innerHTML = '';
+
+            for (let index = 0; index < shoppingItems.length; index += 1) {
+                document.getElementById('list').innerHTML += '<li>' + shoppingItems[index] +
+                ' <button onclick="removeItem(' + index + '); updateList();">X</button></li>';
+            }
+        }
